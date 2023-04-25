6dffae6cf9465efe0ac6da672e6e01a3ccfa300e
Berlin Experiment <info@berlin-experiment.com>
2023-04-25 14:05:13 +0200

    removed comments

--- mobile/lib/time_form.dart
+++ mobile/lib/time_form.dart
@@ -63,10 +63,7 @@ class _TimeFormState extends State<TimeForm> {
               padding: const EdgeInsets.symmetric(vertical: 16.0),
               child: ElevatedButton(
                 onPressed: () {
-                  // Validate returns true if the form is valid, or false otherwise.
                   if (_formKey.currentState!.validate()) {
-                    // If the form is valid, display a snackbar. In the real world,
-                    // you'd often call a server or save the information in a database.
                     ScaffoldMessenger.of(context).showSnackBar(
                       const SnackBar(content: Text('Processing Data')),
                     );
